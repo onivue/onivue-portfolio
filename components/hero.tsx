a488356af8780c346e06f8948fc37cb8ed704fea
Albin Hoti <73725192+onivue@users.noreply.github.com>
2025-10-19 00:34:03 +0200

    Replace Github icon with SVG representation

--- components/hero.tsx
+++ components/hero.tsx
@@ -27,7 +27,7 @@ export function Hero() {
                                 rel="noopener noreferrer"
                                 className="border-4 border-black bg-white p-3 shadow-retro-sm hover:translate-x-1 hover:translate-y-1 hover:shadow-none dark:border-white dark:bg-black"
                             >
-                                <Github className="h-6 w-6" />
+                               <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path fill="currentColor" d="M5 2h4v2H7v2H5V2Zm0 10H3V6h2v6Zm2 2H5v-2h2v2Zm2 2v-2H7v2H3v-2H1v2h2v2h4v4h2v-4h2v-2H9Zm0 0v2H7v-2h2Zm6-12v2H9V4h6Zm4 2h-2V4h-2V2h4v4Zm0 6V6h2v6h-2Zm-2 2v-2h2v2h-2Zm-2 2v-2h2v2h-2Zm0 2h-2v-2h2v2Zm0 0h2v4h-2v-4Z"/> </svg>
                             </Link>
                             <Link
                                 href="https://www.linkedin.com/in/albin-hoti-a1991b237"
